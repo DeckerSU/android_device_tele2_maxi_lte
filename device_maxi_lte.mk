64eb11f6653d21d902544b7175f3d1c2f07563dd
DeckerSU <support@decker.su>
2017-02-14 22:39:39 +0300

    added support for 64-bit zygote

--- device_maxi_lte.mk
+++ device_maxi_lte.mk
@@ -1,5 +1,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, build/target/product/aosp_arm64.mk)
 
 # These additionals go to /default.prop
 ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
