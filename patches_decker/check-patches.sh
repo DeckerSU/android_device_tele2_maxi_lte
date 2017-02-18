bc9456fdd91d36327e6e28fda6da825043b83980
DeckerSU <support@decker.su>
2017-02-18 17:27:47 +0300

    add libnetutils MTK bits ifc_ccmni_md_cfg patch

--- patches_decker/check-patches.sh
+++ patches_decker/check-patches.sh
@@ -2,6 +2,7 @@
 cd ../../../..
 cd system/core
 git apply -v --check ../../device/tele2/maxi_lte/patches_decker/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply -v --check ../../device/tele2/maxi_lte/patches_decker/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 cd ../..
 cd bionic
 git apply -v --check ../device/tele2/maxi_lte/patches_decker/0002-Apply-LIBC-version-to-__pthread_gettid.patch
