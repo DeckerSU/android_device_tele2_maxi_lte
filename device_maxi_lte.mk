b9cae53255642c78d65cb3031d78566f5c317b89
DeckerSU <support@decker.su>
2017-02-26 04:37:29 +0300

    - disable NFC & Smartcover in build - clean system.prop from comments

--- device_maxi_lte.mk
+++ device_maxi_lte.mk
@@ -152,26 +152,26 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
-# NFC
-PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    nfcstackp \
-    libmtknfc_dynamic_load_jni \
-    libnfc_mt6605_jni \
-    Nfc \
-    Tag
+# NFC (disabled in this build)
+#PRODUCT_PACKAGES += \
+#    com.android.nfc_extras \
+#    nfcstackp \
+#    libmtknfc_dynamic_load_jni \
+#    libnfc_mt6605_jni \
+#    Nfc \
+#    Tag
 
-PRODUCT_COPY_FILES += \
-    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+#PRODUCT_COPY_FILES += \
+#    packages/apps/Nfc/migrate_nfc.txt:system/etc/updatecmds/migrate_nfc.txt \
+#    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+#    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+#    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
 # Torch
 PRODUCT_PACKAGES += \
     Torch
 
-# FM Radio
+# FM Radio (disabled in this build)
 PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni \
@@ -184,8 +184,8 @@ PRODUCT_COPY_FILES += \
 # libmtkplayer description in vendor (!)
 
 # Smart Cover
-PRODUCT_PACKAGES += \
-    SmartCover
+#PRODUCT_PACKAGES += \
+#    SmartCover
 
 # Camera
 PRODUCT_PACKAGES += \
